d49a814c2aaa74a4b2bb7414e0855240dfcf4495
Administrator <243237049@qq.com>
2023-01-10 14:49:18 +0800

    q

--- app/src/main/java/com/demo/securewifianalyzer/app/App.kt
+++ app/src/main/java/com/demo/securewifianalyzer/app/App.kt
@@ -2,12 +2,15 @@ package com.demo.securewifianalyzer.app
 
 import android.app.Application
 import com.demo.securewifianalyzer.manager.ActivityCallback
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import com.tencent.mmkv.MMKV
 import org.litepal.LitePal
 
 class App:Application() {
     override fun onCreate() {
         super.onCreate()
+        Firebase.initialize(this)
         LitePal.initialize(this)
         MMKV.initialize(this)
         ActivityCallback.register(this)
